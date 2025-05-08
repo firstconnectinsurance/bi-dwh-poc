da2b54c3e37e417e793e4bcfdbf4d96227c12af9
avi-fcis <avi@fcis.com>
2025-05-08 13:44:46 +0300

    1st commit

---
+++ my_dbt_project/models/example/src/src_appetite_finder_sessions.sql
@@ -0,0 +1,16 @@
+WITH src_appetite_finder_sessions AS (
+ SELECT
+ *
+ FROM
+ poc_hevo.portal_hevo.appetite_finder_sessions
+)
+SELECT
+ id AS appetite_finder_sessions_id,
+ producer_id,
+lob,
+ status,
+ created_at,
+ state,
+ product,
+FROM
+ src_appetite_finder_sessions
